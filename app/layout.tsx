1db5fedeb5c33134f8ca227756aa33bd8b314f46
afifaimrann <afifaimran844@gmail.com>
2025-01-26 14:09:51 +0600

    modified files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,16 @@
-import type { Metadata } from "next";
+import type { Metadata } from 'next';
 import { Poppins } from 'next/font/google';
+import './globals.css';
 
-//import { GeistSans, GeistMono } from '@next/font/google';
-import "./globals.css";
-
-
-const poppins = Poppins( {
+const poppins = Poppins({
   subsets: ['latin'],
-  weight: ['100' , '200' , '300' , '400' , '500' , '600' , '700' , '800' , '900'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
   variable: '--font-poppins',
-
-})
+});
 
 export const metadata: Metadata = {
-  title: "StoreIt",
-  description: "StoreIt- The only storage solution you need.",
+  title: 'StoreIt',
+  description: 'StoreIt- The only storage solution you need.',
 };
 
 export default function RootLayout({
@@ -24,9 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${poppins.variable} font-poppins antialiased`}
-      >
+      <body className={`${poppins.variable} font-poppins antialiased`}>
         {children}
       </body>
     </html>
