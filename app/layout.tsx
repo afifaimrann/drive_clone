6d3bb374c66009f466c83acc7851644a2fe510e2
afifaimrann <afifaimran844@gmail.com>
2025-01-28 14:11:16 +0600

    sign-up and sign-in work

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,16 @@
-import type { Metadata } from 'next';
-import { Poppins } from 'next/font/google';
-import './globals.css';
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
 
 const poppins = Poppins({
-  subsets: ['latin'],
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-  variable: '--font-poppins',
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-poppins",
 });
 
 export const metadata: Metadata = {
-  title: 'StoreIt',
-  description: 'StoreIt- The only storage solution you need.',
+  title: "StoreIt",
+  description: "StoreIt- The only storage solution you need.",
 };
 
 export default function RootLayout({
